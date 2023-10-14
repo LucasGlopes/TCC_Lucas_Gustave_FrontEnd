4e6f8ca6bf6de9952247ef2763273aa8d8c6d1d7
LucasGlopes <lucasglpes@gmail.com>
2023-10-13 23:51:11 -0300

    fix: agendamento aparece apenas para técnico

--- src/app/features/vaccination/vaccination-home/vaccination-home.component.ts
+++ src/app/features/vaccination/vaccination-home/vaccination-home.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Perfis } from 'src/app/models/user.model';
+import { CurrentUserService } from 'src/app/services/currentUser.service';
 
 @Component({
-  selector: 'app-vaccination-home',
-  templateUrl: './vaccination-home.component.html',
-  styleUrls: ['./vaccination-home.component.scss']
+	selector: 'app-vaccination-home',
+	templateUrl: './vaccination-home.component.html',
+	styleUrls: ['./vaccination-home.component.scss']
 })
-export class VaccinationHomeComponent {
+export class VaccinationHomeComponent implements OnInit{
+	hasPermission:boolean = false;
 
+	constructor(
+		private user: CurrentUserService
+	){}
+
+	ngOnInit(): void {
+		this.checkPermission();
+	}
+
+	checkPermission(){
+		const profiles = this.user.getUserValues().perfis;
+
+		this.hasPermission = profiles.some(profile => 
+			profile === Perfis.admin || profile === Perfis.tecnico
+		);
+	}
 }
 
