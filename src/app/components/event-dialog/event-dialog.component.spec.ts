2b1ec07de651a17b61c0e508f7994782062335e3
LucasGlopes <lucasglpes@gmail.com>
2023-11-19 18:45:36 -0300

    tests: Adição de testes de dialogs

--- src/app/components/event-dialog/event-dialog.component.spec.ts
+++ src/app/components/event-dialog/event-dialog.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventDialogComponent } from './event-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('EventDialogComponent', () => {
   let component: EventDialogComponent;
@@ -8,7 +9,14 @@ describe('EventDialogComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EventDialogComponent]
+      declarations: [EventDialogComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { 
+          provide: MatDialogRef, 
+          useValue: {}
+        }
+      ]
     });
     fixture = TestBed.createComponent(EventDialogComponent);
     component = fixture.componentInstance;
