0cb1854d08afc68ba0bbdfae1636fc83ebfc7428
LucasGlopes <lucasglpes@gmail.com>
2023-11-19 18:45:15 -0300

    tests: Adição de testes de login e cadastro

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CurrentUserService } from './services/currentUser.service';
+import { Observable } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
+    providers: [
+      CurrentUserService
+    ],
     declarations: [AppComponent]
   }));
 
@@ -12,6 +17,20 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should set is LoggedIn', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const testObservable = new Observable();
+
+    const userService = TestBed.inject(CurrentUserService);
+    spyOnProperty(userService, 'isLoggedIn', 'get').and.returnValue(testObservable);
+
+    fixture.detectChanges(); 
+
+    expect(app.isLoggedIn$).toBe(testObservable);
   });
 
 });
